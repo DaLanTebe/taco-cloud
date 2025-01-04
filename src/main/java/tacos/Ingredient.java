ad3251239381c931de2f0da867da5b43a3d2ceb2
daniil <danilka.radaev.00@mail.ru>
2025-01-04 20:08:48 +0300

    Taco-cloud 1.3

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -2,13 +2,15 @@ package tacos;
 
 import lombok.Data;
 
+
 @Data
 public class Ingredient {
 
     private final String id;
     private final String name;
     private final Type type;
+
     public enum Type {
-        Wrap, Protein, Veggies, Cheese, Sauce
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
     }
 }
--- src/test/java/sia/tacocloud/HomeControllerTest.java
+++ src/test/java/sia/tacocloud/HomeControllerTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
+import tacos.web.HomeController;
 
 @WebMvcTest(HomeController.class)
 public class HomeControllerTest {
@@ -22,7 +23,6 @@ public class HomeControllerTest {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("home"))
-                .andExpect(content().string(
-                        containsString("Welcome to...")));
+                .andExpect(content().string(containsString("Welcome to...")));
     }
 }
