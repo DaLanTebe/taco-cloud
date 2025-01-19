6991a27f7704dd011e18e8da4688e53df3d62576
danaradaev <danilka.radaev.00@mail.ru>
2025-01-19 20:40:58 +0300

    Taco-cloud 2.1 (запросы в бд)

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -4,10 +4,16 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
+import java.util.Date;
 import java.util.List;
 
 @Data
 public class Taco {
+
+    private Long id;
+
+    private Date createdAt = new Date();
+
 
     @NotNull
     @Size(min = 3, message = "Имя должно быть как минимум из 3 букв")
--- src/main/java/tacos/TacoOrder.java
+++ src/main/java/tacos/TacoOrder.java
@@ -7,10 +7,15 @@ import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Data
 public class TacoOrder {
+
+    private static final long serialVersionUID = 1;
+    private Long id;
+    private Date placedAt;
 
     @NotBlank(message = "Delivery name is required")
     private String deliveryName;
@@ -24,14 +29,14 @@ public class TacoOrder {
     private String deliveryZip;
     @CreditCardNumber(message = "Not a valid credit card number")
     private String ccNumber;
-    @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$", message = "must be formatted MM/YY")
+    @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$", message = "must be formatted MM/YY")
     private String ccExpiration;
     @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
 
     private List<Taco> tacos = new ArrayList<>();
 
-    public void addTaco(Taco taco){
+    public void addTaco(Taco taco) {
         this.tacos.add(taco);
     }
 }
