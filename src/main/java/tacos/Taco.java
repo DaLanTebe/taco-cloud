1af4330d1e96464bb292b605458fdd77f38c384a
daniil <danilka.radaev.00@mail.ru>
2025-01-19 05:37:46 +0300

    Taco-cloud 2.0

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,5 +1,7 @@
 package tacos;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.List;
@@ -7,6 +9,11 @@ import java.util.List;
 @Data
 public class Taco {
 
+    @NotNull
+    @Size(min = 3, message = "Имя должно быть как минимум из 3 букв")
     private String name;
+
+    @NotNull
+    @Size(min = 1, message = "Выберите как минимум 1 ингредиент")
     private List<Ingredient> ingredients;
 }
